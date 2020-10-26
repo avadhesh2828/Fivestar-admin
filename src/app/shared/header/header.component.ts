ca9d8f92186cc2cb717c4fc24456cd0280b145d2
Paridhi Jain <pjain@gammastack.com>
2020-10-26 18:18:51 +0530

    Translate header

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -4,6 +4,8 @@ import { first } from 'rxjs/operators';
 import { AuthService } from '../../services/auth.service';
 import { environment } from '../../../environments/environment';
 import { UserService } from 'src/app/services/user.service';
+import { TranslateService } from '@ngx-translate/core';
+
 
 @Component({
     selector: 'app-header',
@@ -16,10 +18,12 @@ export class HeaderComponent implements OnInit {
     readonly projectName = environment.Project_Name;
     user: any;
     myDate = new Date();
+    imageURL = environment.IMG_URL;
     constructor(
         private authService: AuthService,
         private router: Router,
-        private userService: UserService
+        private userService: UserService,
+        public translate: TranslateService
     ) {
         this.userService.currentUser.subscribe((obj: any) => {
             if (this.userService.sizeOfObject(obj)) {
@@ -29,6 +33,11 @@ export class HeaderComponent implements OnInit {
                 }, 1000);
             }
         });
+    }
+
+    switchLang(lang: string) {
+        this.translate.use(lang);
+        localStorage.setItem('language', lang);
     }
 
     ngOnInit() {
