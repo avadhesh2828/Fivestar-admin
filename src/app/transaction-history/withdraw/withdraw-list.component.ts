e406e8b466a268156d7c97b8b6937f12f3a90907
Paridhi Jain <pjain@gammastack.com>
2020-11-03 15:32:54 +0530

    Add operate history

--- src/app/transaction-history/withdraw/withdraw-list.component.ts
+++ src/app/transaction-history/withdraw/withdraw-list.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 import { TransactionService } from '../../services/transaction.service';
 import { formatDate, formatDateTimeZone, range } from '../../services/utils.service';
-import { PAYMENT_FOR, WITHDRAW_TYPE, STATUS } from '../constants';
+import { PAYMENT_FOR, STATUS } from '../constants';
 import { Constants } from '../../constants';
 import { ToastrService } from 'ngx-toastr';
 import { LoaderService } from '../../shared/loader/loader.service';
@@ -23,22 +23,21 @@ const INITIAL_PARAMS = {
 })
 export class WithdrawListComponent implements OnInit {
   public params: any = { ...INITIAL_PARAMS };
-  public url: string = 'finance/withdraw-list?';
+  public url = 'finance/withdraw-list?';
   public withdrawList = [];
   public totalWithdraws = 0;
   public totalPaginationShow = [];
   public totalPages = 0;
   public error = false;
   public paymentFor = PAYMENT_FOR;
-  public withdrawType = WITHDRAW_TYPE;
   public currency_code = Constants.CURRENCY_CODE;
   public isProcessed = STATUS;
   public formatDateTimeZone = formatDateTimeZone;
   public maxDate = new Date();
   constructor(
     private transactionService: TransactionService,
-     private toastr: ToastrService,
-     private loaderService: LoaderService,
+    private toastr: ToastrService,
+    private loaderService: LoaderService,
   ) { }
 
   ngOnInit() {
@@ -46,12 +45,12 @@ export class WithdrawListComponent implements OnInit {
   }
 
   private createUrl(date) {
-    this.url = 'finance/withdraw-list?';
-    this.url += 'perPage=' + this.params.per_page + '&page=' + this.params.current_page + '&withdraw_type=' + this.params.withdraw_type + '&status=' + this.params.status + '&dates=' + JSON.stringify(date) + '&keyword=' + this.params.keyword;
+    this.url = 'finance/transaction-history?';
+    this.url += 'perPage=' + this.params.per_page + '&page=' + this.params.current_page + '&status=' + this.params.status + '&dates=' + JSON.stringify(date) + '&keyword=' + this.params.keyword;
   }
 
   private getWithdrawHistory() {
-    const date = this.params.dates.length == 2 ? {
+    const date = this.params.dates.length === 2 ? {
       fromdate: `${formatDate(this.params.dates[0])} 00:00:00`,
       todate: `${formatDate(this.params.dates[1])} 23:59:59`,
       time_zone: this.params.dates[0].toString().split(' ')[5],
@@ -59,7 +58,7 @@ export class WithdrawListComponent implements OnInit {
     this.createUrl(date);
     this.transactionService.getWithdraws(this.url)
       .subscribe((response: any) => {
-        if (response.response_code == 200) {
+        if (response.response_code === 200) {
           this.withdrawList = response.data.data;
           this.createPaginationItem(response.data.total);
         } else {
@@ -71,21 +70,20 @@ export class WithdrawListComponent implements OnInit {
       });
   }
 
-  public ChangeStatus(withdraw_details)
-  {
+  public ChangeStatus(withdraw_details) {
     this.loaderService.display(true);
-     this.transactionService.changeStatus({'withdraw_details':withdraw_details})
-        .subscribe((response: any) => {
-          if (response) {
-            this.loaderService.display(false);
-            this.getWithdrawHistory();
-            this.toastr.success(response.message);
-            // this.error = false;
-          }
-        }, (error) => {
-          // this.error = true;
-           this.toastr.error(error.error.global_error);
-        });
+    this.transactionService.changeStatus({ 'withdraw_details': withdraw_details })
+      .subscribe((response: any) => {
+        if (response) {
+          this.loaderService.display(false);
+          this.getWithdrawHistory();
+          this.toastr.success(response.message);
+          // this.error = false;
+        }
+      }, (error) => {
+        // this.error = true;
+        this.toastr.error(error.error.global_error);
+      });
   }
 
 
