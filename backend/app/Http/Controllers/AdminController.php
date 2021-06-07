fc63bd087aa9956a46b3b64a42d965271bb6c4d2
ssen <ssen@gammastack.com>
2021-06-07 17:46:31 +0530

    session expired after change password

--- backend/app/Http/Controllers/AdminController.php
+++ backend/app/Http/Controllers/AdminController.php
@@ -150,6 +150,9 @@ class AdminController extends Controller
             if($user > 0){
                 History::create(['action_for' => 5, 'to_id' => $adminInfo->admin_id, 'from_id' => ($adminInfo->parent_id) ? $adminInfo->parent_id:$adminInfo->admin_id, 'name' => $adminInfo->username, 'description' => 'Password changed successfully', 'last_ip' => \Request::ip(), 'created_at' => date('Y-m-d H:i:s'), 'updated_at' => date('Y-m-d H:i:s')]);
 
+                //clear token
+                $request->user()->token()->delete();
+
                 return response()->json([
                     'response_code'=> 200,
                     'service_name' => 'change_password',
@@ -200,6 +203,9 @@ class AdminController extends Controller
             $user = Admin::where('admin_id', $user_id)->update(['personal_password' => Hash::make($security_password)]);
             if($user > 0){
                 History::create(['action_for' => 6, 'to_id' => $adminInfo->admin_id, 'from_id' => ($adminInfo->parent_id) ? $adminInfo->parent_id:$adminInfo->admin_id, 'name' => $adminInfo->username, 'description' => 'Security password changed successfully', 'last_ip' => \Request::ip(), 'created_at' => date('Y-m-d H:i:s'), 'updated_at' => date('Y-m-d H:i:s')]);
+
+                //clear token
+                $request->user()->token()->delete();
 
                 return response()->json([
                     'response_code'=> 200,
