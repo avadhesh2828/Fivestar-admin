4263bc92e33f61d9ebb4093f219bc13ae2d0b3cf
ssen <ssen@gammastack.com>
2021-06-07 17:36:15 +0530

    create session for 2 hours

--- backend/app/Providers/AuthServiceProvider.php
+++ backend/app/Providers/AuthServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
+use Laravel\Passport\Passport;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -24,6 +25,10 @@ class AuthServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->registerPolicies();
+
+        Passport::routes();
+
+        Passport::personalAccessTokensExpireIn(now()->addHours(1));
 
         // define a admin role 
         Gate::define('isAdmin', function($user) {
